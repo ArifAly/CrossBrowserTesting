3d5aeabaff928b93cabc17b17e5131fbba023ea5
Arif <alyaref555@gmail.com>
2021-10-01 17:00:19 +0500

    Last changes

--- cypress/integration/examples/first.js
+++ cypress/integration/examples/first.js
@@ -1,8 +1,14 @@
 /// <reference types="cypress" /> 
-
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    return false
+    });
 describe('Test suite For Cross Browser Testing', function(){
     it('First test case', function(){
     cy.visit('http://demo.automationtesting.in/')
-   
+    cy.get('#enterimg').click()
+    cy.get('h2').contains('Register')
+
     })
 })
