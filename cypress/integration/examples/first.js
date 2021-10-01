4dff7fd3f7cf83562636aa9d126890d6a7b6833d
Arif <alyaref555@gmail.com>
2021-10-01 15:34:35 +0500

    Added new Url

--- cypress/integration/examples/first.js
+++ cypress/integration/examples/first.js
@@ -2,13 +2,7 @@
 
 describe('Test suite For Cross Browser Testing', function(){
     it('First test case', function(){
-    cy.visit('https://www.seleniumeasy.com/test/')
-    cy.get('.at-cm-no-button').click()
-    cy.get('#btn_basic_example').click()
-    cy.get('.panel-body').click()
-    cy.get('.tree-branch').contains('Input Forms').click()
-    cy.get('.form-control').type('Arif Ali')
-    cy.get('.btn.btn-default').click()
-    
+    cy.visit('http://demo.automationtesting.in/')
+   
     })
 })
