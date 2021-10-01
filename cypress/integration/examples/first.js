4727be1bdbcfcb1cfd112fb7a1e22849903baffb
Arif <alyaref555@gmail.com>
2021-10-01 14:27:41 +0500

    Added

--- cypress/integration/examples/first.js
+++ cypress/integration/examples/first.js
@@ -5,8 +5,8 @@ describe('Test suite For Cross Browser Testing', function(){
     cy.visit('https://www.seleniumeasy.com/test/')
     cy.get('.at-cm-no-button').click()
     cy.get('#btn_basic_example').click()
-    cy.get('#basic').contains('Simple Form Demo').click()
-     
+    cy.get('.panel-body').click()
+     cy.get('.tree-branch').contains('Input Forms').click()
 
     })
 })
