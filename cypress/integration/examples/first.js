9732c786780ed8b72adbe67d87595730016e75cc
Arif <alyaref555@gmail.com>
2021-10-01 12:27:09 +0500

    updated

--- cypress/integration/examples/first.js
+++ cypress/integration/examples/first.js
@@ -2,7 +2,10 @@
 
 describe('Test suite For Cross Browser Testing', function(){
     it('First test case', function(){
-   cy.visit('https://www.seleniumeasy.com/test/')
-   cy.get('.at-cm-no-button').click()
+    cy.visit('https://www.seleniumeasy.com/test/')
+    cy.get('.at-cm-no-button').click()
+    cy.get('#btn_basic_example').click()
+    cy.get('#basic').contains('Simple Form Demo').click()
+
     })
 })
