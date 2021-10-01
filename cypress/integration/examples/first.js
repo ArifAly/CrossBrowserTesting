f7a85743a7881419ebd21193a967cb2e9ecdb412
Arif <alyaref555@gmail.com>
2021-10-01 11:25:09 +0500

    Visit the url

--- cypress/integration/examples/first.js
+++ cypress/integration/examples/first.js
@@ -1,1 +1,8 @@
 /// <reference types="cypress" /> 
+
+describe('Test suite For Cross Browser Testing', function(){
+    it('First test case', function(){
+   cy.visit('https://www.seleniumeasy.com/test/')
+   cy.get('.at-cm-no-button').click()
+    })
+})
