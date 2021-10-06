c1ff45dc76676443a4ff6f9ed044b910b93cf750
Arif <alyaref555@gmail.com>
2021-10-06 17:52:54 +0500

    Second page Changes

---
+++ cypress/integration/examples/second.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress" /> 
+describe('Test Suites for Testing', function(){
+ 
+    it('Test case for the Selectors', function(){
+        cy.visit('http://demo.automationtesting.in/')
+        cy.get('#enterimg').click()
+        cy.get('[href="Interactions.html"]').click()
+        //cy.get(':nth-child(2) > .dropdown-toggle').click()
+        cy.get('.open > .dropdown-menu > :nth-child(2) > a').click()
+        cy.get(':nth-child(2) > .analystic').click()
+        cy.get('.SerializeFunc > :nth-child(1) > b').click() 
+        //cy.get('//span[@id="result"]').contains('Readability')
+         
+    })
+})
+
+
+
+
